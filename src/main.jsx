17d3995d7c09220651d228bf52b1c9f2ba544b14
Thiago <thiagocaffaro223@gmail.com>
2024-03-25 22:22:08 -0300

    hashRouter corrigido e arquivo app modificado para router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App.jsx'
+import AppRouter from './router.jsx'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
-        <App></App>
-    </HashRouter>
+    <AppRouter></AppRouter>
 )
---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Book from './components/book/book'
+import Teste from './components/teste';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+function AppRouter() {
+
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" exact element={Book}/>
+            <Route path="/teste" element={Teste}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
